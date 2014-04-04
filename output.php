9d1477b907c828a40ede72b00b87d26f04b31742
Aalekh Nigam <aalekh1993@rediffmail.com>
2014-04-04 18:58:44 +0530

    Update output.php

--- output.php
+++ output.php
@@ -1,6 +1,6 @@
 <?php
 
-$arr_for_categories=array("human","animals","machines");
+$arr_for_categories=array("plants","animals","machines");
 $arr_rand_categories = array();
 $count_categories = count($arr_for_categories);
 
