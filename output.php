47dddfd62992dbaee3d87e9694c40cee14120865
Aalekh Nigam <aalekh1993@rediffmail.com>
2014-04-04 18:55:44 +0530

    Update output.php
    Category insertion included

--- output.php
+++ output.php
@@ -1,9 +1,33 @@
 <?php
-$json = file_get_contents('https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:machines&prop=imageinfo&gcmlimit=100&iiprop=url&format=json&indexpageids', true);
+
+$arr_for_categories=array("human","animals","machines");
+$arr_rand_categories = array();
+$count_categories = count($arr_for_categories);
+
+
+for ($x=0; $x<2; $x++)
+  {
+  	$rand_category = mt_rand(0, $count_categories -1);
+    if (!in_array($arr_for_categories[$rand_category], $arr_rand_categories))
+  	{
+  		array_push($arr_rand_categories, $arr_for_categories[$rand_category]);
+  	}
+  	else{
+
+  		$rand_category = rand(0, $a_two-1);
+  		array_push($arr_rand_categories, $arr_for_categories[$rand_category]);
+  	}
+	
+  } 
+
+$url_one = 'https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:'.urlencode($arr_rand_categories[0]).'&prop=imageinfo&gcmlimit=100&iiprop=url&format=json&indexpageids';
+$url_two = 'https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:'.urlencode($arr_rand_categories[1]).'&prop=imageinfo&gcmlimit=100&iiprop=url&format=json&indexpageids';
+
+$json = file_get_contents($url_one, true);
 $obj = json_decode($json);
 $a =  count($obj->query->pageids);
  
-$json_two = file_get_contents('https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:animals&prop=imageinfo&gcmlimit=100&iiprop=url&format=json&indexpageids', true);
+$json_two = file_get_contents($url_two, true);
 $obj_two = json_decode($json_two);
 $a_two =  count($obj_two->query->pageids);
 
